cd0eb3329e19799d81e47aa3ae811da10a3bc801
gabrielecv <100017598+gabrielecv@users.noreply.github.com>
2022-11-21 10:40:40 +0100

    Update seller_panel_scripts.js

--- seller_panel_scripts.js
+++ seller_panel_scripts.js
@@ -12,12 +12,12 @@ var tidioScript = document.createElement('script');
 tidioScript.src = '//code.tidio.co/cy97akcfmtocrapn5plbm9d2bdvizjd7.js';
 document.body.appendChild(tidioScript);
 
-var fontPoppins = `
+/*var fontPoppins = `
  <link rel="preconnect" href="https://fonts.googleapis.com">
  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 `;
-document.body.innerHTML += fontPoppins;
+document.body.innerHTML += fontPoppins;*/
 
 if (document.getElementById('store_name') !== null) {
   document.getElementById('store_name').readOnly= true;
