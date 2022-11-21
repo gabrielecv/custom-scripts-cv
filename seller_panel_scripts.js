3b4f922f697f26190eefe7fe7254cafabe283e03
gabrielecv <100017598+gabrielecv@users.noreply.github.com>
2022-11-21 10:42:12 +0100

    Update seller_panel_scripts.js

--- seller_panel_scripts.js
+++ seller_panel_scripts.js
@@ -13,6 +13,7 @@ tidioScript.src = '//code.tidio.co/cy97akcfmtocrapn5plbm9d2bdvizjd7.js';
 document.body.appendChild(tidioScript);
 
 /*var fontPoppins = `
+prova
  <link rel="preconnect" href="https://fonts.googleapis.com">
  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
